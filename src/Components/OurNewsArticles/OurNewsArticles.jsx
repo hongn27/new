6533b0ec417960fc087e66069c56891714bf4ee0
Hong0rebro <hong.nguyen@utb.ecutbildning.se>
2023-11-19 23:30:56 +0100

    news

--- src/Components/OurNewsArticles/OurNewsArticles.jsx
+++ src/Components/OurNewsArticles/OurNewsArticles.jsx
@@ -22,23 +22,22 @@ function OurNewsArticles() {
   return (
     <div>
       <div className='container'>
-      <ul>
-        {posts.map(post => (
-          <li key={post.id}>{post.title}{post.content}{post.author}{post.published}{post.category}{post.imageUrl} </li>
+        <ul>
+          {posts.map(post => (
+            <li key={post.id}>{post.title}{post.content}{post.author}{post.published}{post.category}{post.imageUrl} </li>
             ))}
-      </ul>
+        </ul>
 
-      <div className="pagesContent border">
-        
-                <Link to="/"><i className="fa-solid fa-angle-left"></i></Link>
-                <Link className="active" to="/">1</Link>
-                <Link to="/service/page2">2</Link>
-                <Link to="/News/page3">3</Link>
-                <Link to="/News/page">...</Link>
-                <Link to="/News/page9">9</Link>
-                <Link to="/News/page"><i className="fa-solid fa-angle-right"></i></Link>
-            </div>
-        </div>   
+        <div className="pagesContent border">
+            <Link to="/"><i className="fa-solid fa-angle-left"></i></Link>
+            <Link className="active" to="/">1</Link>
+            <Link to="/service/page2">2</Link>
+            <Link to="/News/page3">3</Link>
+            <Link to="/News/page">...</Link>
+            <Link to="/News/page9">9</Link>
+            <Link to="/News/page"><i className="fa-solid fa-angle-right"></i></Link>
+          </div>
+      </div>   
     </div>
     
   )
